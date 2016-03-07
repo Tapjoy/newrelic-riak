9340b1db97b9b58a93d8813ce2db8d157733cea5
StabbyCutyou <sean.kelly@tapjoy.com>
2016-03-07 14:14:09 -0500

    Updating the newrelic riak gem to use the new DataStore tracers

--- lib/newrelic_riak/riak_client.rb
+++ lib/newrelic_riak/riak_client.rb
@@ -1,5 +1,6 @@
 require 'new_relic/agent/method_tracer'
 
+
 DependencyDetection.defer do
   @name = :riak_client
 
@@ -12,66 +13,82 @@ DependencyDetection.defer do
   end
 
   executes do
-    backend_tracers = proc do
-      add_method_tracer :ping, 'Database/Riak/ping'
 
-      add_method_tracer :list_buckets, 'Database/Riak/list_buckets'
-      add_method_tracer :get_bucket_props, 'Database/Riak/get_bucket_props'
-      add_method_tracer :set_bucket_props, 'Database/Riak/set_bucket_props'
+    # Make this bad boy re-usable
+    backend_tracers = Proc.new do |klass|
+      NewRelic::Agent::Datastores.trace klass, :ping
 
-      add_method_tracer :mapred, 'Database/Riak/mapred'
+      NewRelic::Agent::Datastores.trace klass, :list_buckets
+      NewRelic::Agent::Datastores.trace klass, :get_bucket_props
+      NewRelic::Agent::Datastores.trace klass, :set_bucket_props
 
-      add_method_tracer :list_keys, 'Database/Riak/list_keys'
-      add_method_tracer :reload_object, 'Database/Riak/reload_object'
-      add_method_tracer :delete_object, 'Database/Riak/delete_object'
+      NewRelic::Agent::Datastores.trace klass, :mapred
+
+      NewRelic::Agent::Datastores.trace klass, :list_keys
+      NewRelic::Agent::Datastores.trace klass, :reload_object
+      NewRelic::Agent::Datastores.trace klass, :delete_object
     end
+
+    get_set_callback = Proc.new do |result, scoped_metric, elapsed|
+      NewRelic::Agent::Datastores.notice_statement(query, scoped_metric, elapsed)
+    end
+
+    # Instrument measuring the store_object Callbacks
+
+    # Instrument the ProtobuffsBackend
+    backend_tracers ::Riak::Client::BeefcakeProtobuffsBackend
+    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :server_info
+    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :get_client_id
+    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :set_client_id
+    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :get_index
+
+    # Instrument the HTTPBackend
+    backend_tracers ::Riak::Client::HTTPBackend
+    NewRelic::Agent::Datastores.trace ::Riak::Client::HTTPBackend, :stats
+    NewRelic::Agent::Datastores.trace ::Riak::Client::HTTPBackend, :link_walk
+    NewRelic::Agent::Datastores.trace ::Riak::Client::HTTPBackend, :get_index
+    NewRelic::Agent::Datastores.trace ::Riak::Client::HTTPBackend, :search
+    NewRelic::Agent::Datastores.trace ::Riak::Client::HTTPBackend, :update_search_index
+
+    # Instrument RObject
+    NewRelic::Agent::Datastores.trace ::Riak::RObject, :serialize
 
     ::Riak::Client.class_eval do
 
       def store_object_with_newrelic_trace(*args, &blk)
+        total_metric = 'ActiveRecord/allOther'
         if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
           total_metric = 'ActiveRecord/allWeb'
-        else
-          total_metric = 'ActiveRecord/allOther'
         end
 
         robject = args[0].is_a?(Array) ? args[0][0] : args[0]
         bucket = robject.respond_to?(:bucket) && robject.bucket ? robject.bucket.name : ''
         bucket = self.newrelic_riak_camelize(bucket)
-        metrics = ["ActiveRecord/#{bucket}/save", total_metric, 'ActiveRecord/#{bucket}', 'ActiveRecord/save', 'ActiveRecord/all']
 
-        self.class.trace_execution_scoped(metrics) do
-          start = Time.now
-
+        NewRelic::Agent::Datastores.wrap("Riak", "save", bucket, get_set_callback) do
           begin
             store_object_without_newrelic_trace(*args, &blk)
-          ensure
-            s = NewRelic::Agent.instance.transaction_sampler
-            s.notice_nosql(args.inspect, (Time.now - start).to_f) rescue nil
+          rescue => e
+            # NOOP apparently?
           end
         end
       end
 
       def get_object_with_newrelic_trace(*args, &blk)
+        total_metric = 'ActiveRecord/allOther'
         if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
           total_metric = 'ActiveRecord/allWeb'
-        else
-          total_metric = 'ActiveRecord/allOther'
         end
 
         bucket = args[0].is_a?(Array) ? args[0][0] : args[0]
         bucket = bucket && bucket.respond_to?(:name) ? bucket.name : bucket.to_s
         bucket = self.newrelic_riak_camelize(bucket)
-        metrics = ["ActiveRecord/#{bucket}/find", total_metric, 'ActiveRecord/#{bucket}', 'ActiveRecord/find', 'ActiveRecord/all']
 
-        self.class.trace_execution_scoped(metrics) do
-          start = Time.now
-
+        NewRelic::Agent::Datastores.wrap("Riak", "find", bucket, get_set_callback) do
           begin
             get_object_without_newrelic_trace(*args, &blk)
-          ensure
-            s = NewRelic::Agent.instance.transaction_sampler
-            s.notice_nosql(args.inspect, (Time.now - start).to_f) rescue nil
+          rescue => e
+            # NOOP apparently?
           end
         end
       end
@@ -88,24 +105,5 @@ DependencyDetection.defer do
       end
     end
 
-    ::Riak::Client::BeefcakeProtobuffsBackend.class_eval &backend_tracers
-    ::Riak::Client::BeefcakeProtobuffsBackend.class_eval do
-      add_method_tracer :server_info, 'Database/Riak/server_info'
-      add_method_tracer :get_client_id, 'Database/Riak/get_client_id'
-      add_method_tracer :set_client_id, 'Database/Riak/set_client_id'
-      add_method_tracer :get_index, 'Database/Riak/get_index'
-    end
-    ::Riak::Client::HTTPBackend.class_eval &backend_tracers
-    ::Riak::Client::HTTPBackend.class_eval do
-      add_method_tracer :stats, 'Database/Riak/stats'
-      add_method_tracer :link_walk, 'Database/Riak/link_walk'
-      add_method_tracer :get_index, 'Database/Riak/get_index'
-      add_method_tracer :search, 'Database/Riak/search'
-      add_method_tracer :update_search_index, 'Database/Riak/update_search_index'
-    end
-
-    ::Riak::RObject.class_eval do
-      add_method_tracer :serialize, 'Database/Riak/serialize'
-    end
   end
 end
--- newrelic_riak.gemspec
+++ newrelic_riak.gemspec
@@ -7,10 +7,9 @@ Gem::Specification.new do |gem|
   gem.email = ["alin.popa@gmail.com"]
   gem.homepage = "https://github.com/alinpopa/newrelic-riak"
   gem.summary = "NewRelic instrumentation for Riak."
-  
-  gem.add_runtime_dependency(%q<newrelic_rpm>, ["~> 3.0"])
-  
+
+  gem.add_runtime_dependency(%q<newrelic_rpm>, ["~> 3.15"])
+
   gem.files = ["README.md", "lib/newrelic_riak.rb", "lib/newrelic_riak/riak_client.rb", "lib/newrelic_riak/ripple.rb", "newrelic_riak.gemspec"]
   gem.require_paths = ['lib']
 end
-
