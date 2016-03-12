e58338319253fbd6d4e5c89319511898cda3b206
Brent Stephens <brent.stephens@tapjoy.com>
2016-03-11 16:45:19 -0800

    OPER-2081 OPER-2082 First working version of updated gem

--- lib/newrelic_riak/riak_client.rb
+++ lib/newrelic_riak/riak_client.rb
@@ -1,3 +1,5 @@
+require 'new_relic/agent/datastores'
+
 DependencyDetection.defer do
   @name = :riak_client
 
@@ -6,94 +8,84 @@ DependencyDetection.defer do
   end
 
   executes do
-    NewRelic::Agent.logger.debug 'Installing Riak client instrumentation'
+    NewRelic::Agent.logger.info 'Installing Riak client instrumentation'
   end
 
   executes do
-    NewRelic::Agent.logger.debug "POOP - Beginning"
-    get_set_callback = Proc.new do |result, scoped_metric, elapsed|
-      NewRelic::Agent.logger.debug "POOP - GSC"
+    # Make this bad boy re-usable
+    NewRelic::Agent.logger.info 'Riak: backend_tracers'
+    backend_tracers = Proc.new do |klass, klass_methods, product = 'Riak'|
+      klass_methods.each do |entry|
+        NewRelic::Agent::Datastores.trace klass, entry, product
+      end
+    end
+
+    # Instrument the ProtoBuffsBackend
+    NewRelic::Agent.logger.info 'Riak: BeefcakeProtoBuffsBackend'
+    method_list = %w(ping list_buckets get_bucket_props set_bucket_props mapred list_keys
+                     reload_object delete_object server_info get_client_id set_client_id get_index)
+    backend_tracers.call(::Riak::Client::BeefcakeProtobuffsBackend, method_list)
+    
+    # Instrument RObject
+    NewRelic::Agent.logger.info 'Riak: RObject'
+    backend_tracers.call(::Riak::RObject, ['serialize'])
+
+    @get_set_callback = Proc.new do |result, scoped_metric, elapsed|
       NewRelic::Agent::Datastores.notice_statement(query, scoped_metric, elapsed)
     end
 
-    # Instrument measuring the store_object Callbacks
-
-    # Instrument the ProtobuffsBackend
-    NewRelic::Agent.logger.debug "POOP - BeefcakeProtobuffsBackend"
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :ping
-
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :list_buckets
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :get_bucket_props
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :set_bucket_props
-
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :mapred
-
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :list_keys
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :reload_object
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :delete_object
-
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :server_info
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :get_client_id
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :set_client_id
-    NewRelic::Agent::Datastores.trace ::Riak::Client::BeefcakeProtobuffsBackend, :get_index
-
-    # Instrument RObject
-    NewRelic::Agent.logger.debug "POOP - RObject"
-    NewRelic::Agent::Datastores.trace ::Riak::RObject, :serialize
-
+    # Instrument Riak client get/store
+    NewRelic::Agent.logger.info 'Riak: Wrap Riak::Client save/find with NR and alias them'
     ::Riak::Client.class_eval do
-    NewRelic::Agent.logger.debug "POOP - Client class_eval"
-      def store_object_with_newrelic_trace(*args, &blk)
-        total_metric = 'ActiveRecord/allOther'
-        if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
-          total_metric = 'ActiveRecord/allWeb'
-        end
-
-        robject = args[0].is_a?(Array) ? args[0][0] : args[0]
-        bucket = robject.respond_to?(:bucket) && robject.bucket ? robject.bucket.name : ''
-        bucket = self.newrelic_riak_camelize(bucket)
-
-        NewRelic::Agent::Datastores.wrap("Riak", "save", bucket, get_set_callback) do
-          begin
-            store_object_without_newrelic_trace(*args, &blk)
-          rescue => e
-            # NOOP apparently?
-            nil
-          end
-        end
-      end
-
-      def get_object_with_newrelic_trace(*args, &blk)
-        total_metric = 'ActiveRecord/allOther'
-        if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
-          total_metric = 'ActiveRecord/allWeb'
-        end
-
-        bucket = args[0].is_a?(Array) ? args[0][0] : args[0]
-        bucket = bucket && bucket.respond_to?(:name) ? bucket.name : bucket.to_s
-        bucket = self.newrelic_riak_camelize(bucket)
-
-        NewRelic::Agent::Datastores.wrap("Riak", "find", bucket, get_set_callback) do
-          begin
-            get_object_without_newrelic_trace(*args, &blk)
-          rescue => e
-            # NOOP apparently?
-            nil
-          end
-        end
-      end
-      NewRelic::Agent.logger.debug "POOP - ALIASING"
+      include NewRelic::Agent::Instrumentation::Riak
       alias_method :store_object_without_newrelic_trace, :store_object
       alias_method :store_object, :store_object_with_newrelic_trace
       alias_method :get_object_without_newrelic_trace, :get_object
       alias_method :get_object, :get_object_with_newrelic_trace
+    end
+  end
+end
 
-      # turn bucket-name-for-things_that_ar-ok into BucketNameForThingsThatArOk
-      def newrelic_riak_camelize(term)
-        string = term.to_s.capitalize
-        string.gsub(/[_-]([a-z]*)/) { "#{$1.capitalize}" }
+module NewRelic
+  module Agent
+    module Instrumentation
+      module Riak
+        # turn bucket-name-for-things_that_ar-ok into BucketNameForThingsThatArOk
+        def newrelic_riak_camelize(term)
+          string = term.to_s.capitalize
+          string.gsub(/[_-]([a-z]*)/) { "#{$1.capitalize}" }
+        end
+
+        def store_object_with_newrelic_trace(*args, &blk)
+          robject = args[0].is_a?(Array) ? args[0][0] : args[0]
+          bucket = robject.respond_to?(:bucket) && robject.bucket ? robject.bucket.name : ''
+          bucket = self.newrelic_riak_camelize(bucket)
+
+          NewRelic::Agent::Datastores.wrap('Riak', 'save', bucket, @get_set_callback) do
+            begin
+              store_object_without_newrelic_trace(*args, &blk)
+            rescue => e
+              # NOOP apparently?
+              nil
+            end
+          end
+        end
+
+        def get_object_with_newrelic_trace(*args, &blk)
+          bucket = args[0].is_a?(Array) ? args[0][0] : args[0]
+          bucket = bucket && bucket.respond_to?(:name) ? bucket.name : bucket.to_s
+          bucket = self.newrelic_riak_camelize(bucket)
+
+          NewRelic::Agent::Datastores.wrap('Riak', 'find', bucket, @get_set_callback) do
+            begin
+              get_object_without_newrelic_trace(*args, &blk)
+            rescue => e
+              # NOOP apparently?
+              nil
+            end
+          end
+        end
       end
     end
-
   end
 end
